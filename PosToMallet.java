2931c4633c0acdfb974b616a8d4f04170f45444c
Madhura Parikh <madhuraparikh@gmail.com>
2014-02-23 18:49:26 -0600

    problematic regex

--- PosToMallet.java
+++ PosToMallet.java
@@ -8,32 +8,50 @@ import java.util.regex.Matcher;
 public class PosToMallet {
 	
 	public static void main(String[] args){
+		//System.out.println("hello World");
+		//System.out.println(args[0]);
 		try{
-			Pattern keep = Pattern.compile("\\s+()/()\\s+");
+			Pattern keep = Pattern.compile("\\[\\s*(.+?)/(.+?)\\s+");
+			Pattern keep2 = Pattern.compile("\\s*(.+)/(.+)\\s+");
 			Pattern discard = Pattern.compile("\\[\\s*@.+/CD\\s*\\]");
 			Pattern newLine = Pattern.compile("=+");
-			String outFileName = args[0].substring(0, args[0].indexOf("."));
+			String outFileName = args[0].substring(0, args[0].indexOf(".")) + ".txt";
 			FileWriter fstream = new FileWriter(outFileName);
 			BufferedWriter outFile = new BufferedWriter(fstream);
 			BufferedReader inFile = new BufferedReader(new FileReader(args[0]));
 			String line = null;
 			while((line = inFile.readLine()) != null){
+				//System.out.println("here");
+				System.out.println(line);
 				Matcher mKeep = keep.matcher(line);
+				Matcher mKeep2 =  keep2.matcher(line);
 				Matcher mDiscard = discard.matcher(line);
 				Matcher mNewLine = newLine.matcher(line);
-				if(mNewLine.find())
+				if(mNewLine.find()){
+					System.out.println("in newline");
 					outFile.write("\n");
-				else if(mDiscard.find())
-					continue;
-				else while(mKeep.find()){
-					outFile.write(mKeep.group(1) + " " + mKeep.group(2) + "\n");
 				}
+				else if(mDiscard.find()){
+					System.out.println("in discard");
+					continue;
+				}
+				else if (mKeep.find()){
+					System.out.println("here");
+					outFile.write(mKeep.group(1) + " " + mKeep.group(2) + "\n");
+					while(mKeep.find())
+						outFile.write(mKeep.group(1) + " " + mKeep.group(2) + "\n");
+				}
+				else while (mKeep2.find()){
+                                        System.out.println("here");
+                                        outFile.write(mKeep2.group(1) + " " + mKeep2.group(2) + "\n");                 
+                                }
+
 			}
 			inFile.close();
 			outFile.close();
 		}
 		catch (Exception e){
-			
+		    e.printStackTrace(System.out);		
 		}
 		
 	}
