92a604746ac4a91397ae9c996fcc95b4998e518c
Madhura <madhuraparikh@gmail.com>
2014-02-25 19:51:53 -0600

    added vocabulary to parameter

--- mallet-2.0.7/src/cc/mallet/fst/TokenAccuracyEvaluator.java
+++ mallet-2.0.7/src/cc/mallet/fst/TokenAccuracyEvaluator.java
@@ -52,7 +52,7 @@ public class TokenAccuracyEvaluator extends TransducerEvaluator
 		this (new InstanceList[] {instanceList1, instanceList2, instanceList3}, new String[] {description1, description2, description3});
 	}
 
-	public void evaluateInstanceList (TransducerTrainer trainer, InstanceList instances, String description) 
+	public void evaluateInstanceList (TransducerTrainer trainer, InstanceList instances, String description, HashSet vocabulary) 
   {
 		int numCorrectTokens;
 		int totalTokens;
